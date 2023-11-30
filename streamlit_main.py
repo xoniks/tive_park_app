a9d434c575871464bb2c82885f8f8167751791d5
Egezon Baruti <63745689+xoniks@users.noreply.github.com>
2023-11-30 21:01:55 +0000

    automatic reset

--- streamlit_main.py
+++ streamlit_main.py
@@ -16,11 +16,7 @@ PARKING_LOTS_FILE = "parking_lots.json"
 def load_parking_lots():
     try:
         with open(PARKING_LOTS_FILE, "r") as file:
-            content = file.read()
-            if content:
-                return json.loads(content)
-            else:
-                return {f"Lot {i + 1}": None for i in range(10)}
+            return json.load(file)
     except FileNotFoundError:
         return {f"Lot {i + 1}": None for i in range(10)}
 
@@ -31,7 +27,7 @@ def save_parking_lots(parking_lots):
 def reset_parking_lots():
     # Reset parking lots at 20:35 every day
     now = datetime.datetime.now()
-    reset_time = datetime.datetime(now.year, now.month, now.day, 17, 35, 0)
+    reset_time = datetime.datetime(now.year, now.month, now.day, 21, 15, 0)
 
     if now >= reset_time:
         return {f"Lot {i + 1}": None for i in range(10)}
@@ -41,7 +37,6 @@ def reset_parking_lots():
 parking_lots = load_parking_lots()
 
 def reset_parking_lots_daily():
-    global parking_lots
     while True:
         reset_result = reset_parking_lots()
         if reset_result:
@@ -53,19 +48,20 @@ def reset_parking_lots_daily():
 
 # Start the thread for daily reset
 reset_thread = threading.Thread(target=reset_parking_lots_daily)
+reset_thread.daemon = True
 reset_thread.start()
 
 def parking_app():
     global parking_lots  # Make parking_lots a global variable
 
-    st.title("Tive Kosovo Parking App")
+    st.title("Parking App")
 
     # Display current time
     current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    st.write(f"Current Time: {current_time}, parking lots will be reset every day at 17:30!")
+    st.subheader(f"Current Time: {current_time}")
 
     # Main content
-    # st.write("Welcome to the Parking App!")
+    st.write("Welcome to the Parking App!")
 
     # Display available parking lots
     st.subheader("Available Parking Lots")
@@ -76,18 +72,18 @@ def parking_app():
     worker_name = st.selectbox("Select Worker", workers, index=0)
 
     # Checkbox for agreement
-    agreement_checkbox = st.checkbox("Pasha baxhanakun s'po e rezervoj para se me parku kerrin!")
+    agreement_checkbox = st.checkbox("I agree to the terms and conditions")
 
     # Book parking lot if the checkbox is checked
-    if agreement_checkbox and st.button("Parked the car!"):
+    if agreement_checkbox and st.button("Book Parking Lot"):
         parking_lots[selected_lot] = worker_name
         save_parking_lots(parking_lots)
-        st.success(f"{worker_name} successfully parked {selected_lot}.")
-    elif not agreement_checkbox and st.button("Parked the car!"):
-        st.warning("Check the box!")
+        st.success(f"{worker_name} successfully booked {selected_lot}.")
+    elif not agreement_checkbox and st.button("Book Parking Lot"):
+        st.warning("Please agree to the terms and conditions before submitting.")
 
     # Display booked parking lots
-    st.subheader("Full Parking Lots")
+    st.subheader("Booked Parking Lots")
     booked_lots = {lot: worker for lot, worker in parking_lots.items() if worker is not None}
     if not booked_lots:
         st.info("No parking lots have been booked yet.")
