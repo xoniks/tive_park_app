7e3983d04b06b3f41f7337e7220defb79287b801
Egezon Baruti <63745689+xoniks@users.noreply.github.com>
2023-11-30 20:46:24 +0000

    done

--- streamlit_main.py
+++ streamlit_main.py
@@ -27,7 +27,7 @@ def save_parking_lots(parking_lots):
 def reset_parking_lots():
     # Reset parking lots at 20:35 every day
     now = datetime.datetime.now()
-    reset_time = datetime.datetime(now.year, now.month, now.day, 20, 35, 0)
+    reset_time = datetime.datetime(now.year, now.month, now.day, 17, 35, 0)
 
     if now >= reset_time:
         return {f"Lot {i + 1}": None for i in range(10)}
@@ -54,14 +54,14 @@ reset_thread.start()
 def parking_app():
     global parking_lots  # Make parking_lots a global variable
 
-    st.title("Parking App")
+    st.title("Tive Kosovo Parking App")
 
     # Display current time
     current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    st.subheader(f"Current Time: {current_time}")
+    st.write(f"Current Time: {current_time}, parking lots will be reset everyday at 17:30 !")
 
     # Main content
-    st.write("Welcome to the Parking App!")
+   # st.write("Welcome to the Parking App!")
 
     # Display available parking lots
     st.subheader("Available Parking Lots")
@@ -75,15 +75,15 @@ def parking_app():
     agreement_checkbox = st.checkbox("I agree to the terms and conditions")
 
     # Book parking lot if the checkbox is checked
-    if agreement_checkbox and st.button("Book Parking Lot"):
+    if agreement_checkbox and st.button("Parked the car!"):
         parking_lots[selected_lot] = worker_name
         save_parking_lots(parking_lots)
-        st.success(f"{worker_name} successfully booked {selected_lot}.")
-    elif not agreement_checkbox and st.button("Book Parking Lot"):
-        st.warning("Please agree to the terms and conditions before submitting.")
+        st.success(f"{worker_name} successfully parked {selected_lot}.")
+    elif not agreement_checkbox and st.button("Parked the car!"):
+        st.warning("Pasha baxhanakun s'po e rezervoj para se me parku kerrin!")
 
     # Display booked parking lots
-    st.subheader("Booked Parking Lots")
+    st.subheader("Full Parking Lots")
     booked_lots = {lot: worker for lot, worker in parking_lots.items() if worker is not None}
     if not booked_lots:
         st.info("No parking lots have been booked yet.")
